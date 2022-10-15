13199d3cc20f1de8042e3fb2313deab1d42ca5e8
André Braga <andre.braga@terceiro-sky.com.br>
2022-10-15 20:27:17 -0300

    Splash screen done

---
+++ app/src/main/java/com/github/abraga/fuzetest/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.github.abraga.fuzetest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Move to the initial screen and finish this
+    }
+}
